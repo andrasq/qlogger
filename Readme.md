1cb28659af2cd00f6ae0a5d2071167ec47d790da
Andras <andras@acm.org>
2014-10-18 01:32:55 -0400

    added Readme, started unit tests, added QLogger.LEVELNAMES and fixed some bugs, added qfputs as a dependency; 0.0.2

---
+++ Readme.md
@@ -0,0 +1,94 @@
+qlogger
+=======
+
+quick nodejs logger and newline delimited data transport
+
+QLogger exports a simplified subset of the traditional logging methods:
+error, info, and debug.  Each log message is appended to the logfile
+as a newline delimited data chunk.
+
+Newline delimited text is a universally compatible, easy-to-parse and
+very very fast way to stream and process data.
+
+Points to keep in mind for general-purpose data transport logfiles:
+  - the logfile might have multiple writers and unix writes are not atomic,
+    ie writes need a mutex
+  - the logfile might be consumed by simple readers, so each write should
+    be a complete newline terminated data item (message)
+  - the logfile might get consumed (renamed or removed), ie must reopen
+    the file periodically
+  - the logfile could be used for high-speed low-latency buffering, so
+    the logfile reopen interval should be pretty short
+
+        QLogger = require('qlogger');
+        logger = new QLogger('info');
+   	logger.addFilter(
+            function(msg, loglevel) {
+                return new Date().toISOString() + " [" + QLogger.LEVELNAMES[loglevel]+ "] " + msg;
+            }
+        );
+        logger.addWriter(process.stdout);
+        logger.info("Hello, world.");
+        logger.error("Done.");
+        logger.debug("debug messages not enabled");
+
+### QLogger( loglevel, [writerSpec] )
+
+Create a logger that will log messages of importance loglevel or above.  It is
+an error if the loglevel is not recognized.  Loglevel can be specified as a
+string 'error', 'info' or 'debug'.  Internally, they are converted to the
+standard unix syslog loglevels 3, 6 and 7.  The higher syslog logging levels
+(emerg, alert, and crit) and warning and notice were deliberately omitted,
+leaving just the three essential message classes:  human attention required,
+useful statistics, and everything available for debugging.
+
+WriterSpec may be a writerObject (see below), or a writer specification
+string to use one of the built-in writers.  If no writerSpec is given,
+the logger will be created without a writer.  It is an error if the
+writer specification is not recognized.  The built-in writers are:
+        file://</path/to/file>          // absolute filepath
+        file://<file/name>              // relative filename
+        stdout://                       // process.stdout
+        stderr://                       // process.stderr
+        tcp://<host>:<port>             // tcp connection
+        udp://<host>:<port>             // datagram
+
+Note: for file:// writers to work, Fputs.FileWriter must be available from the
+[qfputs](https://www.npmjs.org/package/qfputs) package.  Qlogger does not
+(yet) drag in the dependency automatically.
+
+### addWriter( writerObject )
+
+Have the logger write log messages with this object.  The object must
+have a method `write( string, callback )`.  The writer will be called
+with the already formatted log line.  Multiple writers are supported.
+
+### addFilter( filterFunction( message, loglevel ) )
+
+Filter the log message before writing it.  The only message filtering
+built in is to make sure that the message ends in a newline.  Each
+filter is free to modify the log message before
+
+By using filters it is possible to daisy-chain loggers so messages are
+observed and logged to multiple locations.
+
+### error( message )
+
+Log an error message.  Error messages will be logged by all loglevels,
+'error', 'info' and 'debug'.
+
+### info( message )
+
+Log an informational message.  The logger must have loglevel 'info' or
+'debug'.
+
+### debug( message )
+
+Log a debug message.  The logger must have loglevel of 'debug'.
+
+### fflush( callback )
+
+Tries to force all writers to write out any buffered data.  Invokes the
+callback once the writes have all finished.  This is a half-hearted
+implementation, since fflush can only flush write streams, tcp sockets and
+FileWriter (file://) objects.
---
+++ test/test-qlogger.js
@@ -0,0 +1,155 @@
+fs = require('fs');
+QLogger = require('../index');
+
+module.exports = {
+    setUp: function(done) {
+        this.lines = [];
+        this.logger = new QLogger('info');
+        done();
+    },
+
+    'class': {
+        'should expose loglevel map class attribute': function(t) {
+            t.ok(QLogger.LOGLEVELS);
+            t.equal(3, QLogger.LOGLEVELS['error']);
+            t.equal(6, QLogger.LOGLEVELS['info']);
+            t.equal(7, QLogger.LOGLEVELS['debug']);
+            t.done();
+        },
+
+        'should expose loglevel names inverse lookup map class attribute': function(t) {
+            t.ok(QLogger.LEVELNAMES);
+            t.equal('error', QLogger.LEVELNAMES[3]);
+            t.equal('info', QLogger.LEVELNAMES[6]);
+            t.equal('debug', QLogger.LEVELNAMES[7]);
+            t.done();
+        },
+
+        'should have createWriter class method': function(t) {
+            t.equal('function', typeof QLogger.createWriter);
+            t.done();
+        },
+    },
+
+    'logging': {
+        setUp: function(done) {
+            this.lines = [];
+            var self = this;
+            this.logger.addWriter({write: function(msg, cb) { self.lines.push(msg); cb(); }});
+            done();
+        },
+
+        'should newline terminate lines': function(t) {
+            this.logger.info("hello");
+            t.equal("hello\n", this.lines[0]);
+            t.done();
+        },
+
+        'should send data to each writer': function(t) {
+            var self = this;
+            this.lines2 = [];
+            this.logger.addWriter({write: function(msg, cb) { self.lines2.push(msg); cb(); }});
+            this.logger.info("hello");
+            t.equal("hello\n", this.lines[0]);
+            t.equal("hello\n", this.lines2[0]);
+            t.done();
+        },
+    },
+
+    'writers': {
+        'should create file://- stdout writer': function(t) {
+            var writer = QLogger.createWriter('file://-');
+            t.equal(process.stdout, writer);
+            t.done();
+        },
+
+        'should create stdout:// writer': function(t) {
+            var writer = QLogger.createWriter('stdout://');
+            t.equal(process.stdout, writer);
+            t.done();
+        },
+
+        'should create stderr:// writer': function(t) {
+            var writer = QLogger.createWriter('stderr://');
+            t.equal(process.stderr, writer);
+            t.done();
+        },
+
+        'should create tcp:// writer': function(t) {
+            var writer = QLogger.createWriter('tcp://ramnode.com:80');
+            t.ok(writer);
+            // tcp sockets are closed with 'end'
+            writer.end();
+            t.done();
+        },
+
+        'should create udp:// writer': function(t) {
+            var writer = QLogger.createWriter('udp://ramnode.com:80');
+            t.ok(writer);
+            // udp sockets are closed with 'close'
+            writer.close();
+            t.done();
+        },
+
+        'should create file:// writer': function(t) {
+            var writer = QLogger.createWriter('file:///tmp/nodeunit.test.out');
+            t.ok(writer);
+            t.done();
+        },
+
+        'should write data to file:// writer': function(t) {
+            var writer = QLogger.createWriter('file:///tmp/nodeunit.test.out');
+            try { fs.unlinkSync('/tmp/nodeunit.test.out'); } catch (e) { }
+            this.logger.addWriter(writer);
+            this.logger.info("hello");
+            t.expect(1);
+            this.logger.fflush(function(err) {
+                if (err) throw err;
+                var contents = fs.readFileSync('/tmp/nodeunit.test.out');
+                fs.unlinkSync('/tmp/nodeunit.test.out');
+                t.equal("hello\n", contents.toString());
+                t.done();
+            });
+        },
+    },
+
+    'filters': {
+        setUp: function(done) {
+            this.lines = [];
+            done();
+        },
+
+        'should pass loglevel to filter': function(t) {
+            var self = this;
+            this.logger.addFilter(function(msg, loglevel) { self.lines.push(loglevel); return msg; });
+            this.logger.info("test1");
+            this.logger.error("test2");
+            t.equal(QLogger.LOGLEVELS['info'], this.lines[0]);
+            t.equal(QLogger.LOGLEVELS['error'], this.lines[1]);
+            t.done();
+        },
+
+        'should apply all filters before writing line': function(t) {
+            var self = this;
+            this.logger.addWriter({write: function(msg, cb) { self.lines.push(msg); cb(); }});
+            this.logger.addFilter(function(msg, loglevel) { return "a<" + msg; });
+            this.logger.addFilter(function(msg, loglevel) { return msg + ">b"; });
+            this.logger.info("hello");
+            this.logger.info("there");
+            t.equals(this.lines[0], "a<hello>b\n");
+            t.equals(this.lines[1], "a<there>b\n");
+            t.done();
+        },
+
+        'should not send info to error-only logger': function(t) {
+            this.logger = new QLogger('error');
+            var self = this;
+            this.logger.addWriter({write: function(msg, cb) { self.lines.push(msg); cb(); }});
+            this.logger.info("info");
+            this.logger.error("error");
+            t.equal(1, this.lines.length);
+            t.equal("error\n", this.lines[0]);
+            t.done();
+        }
+    },
+};
