f566f71cc592f55e877ecba4b09c79ca0184a61b
Andras <andras@acm.org>
2014-11-26 00:40:39 -0500

    expose formatIsoDate in the filters api; 1.2.0

--- filters.js
+++ filters.js
@@ -1,2 +1,5 @@
 module.exports.filterBasic = require('./lib/filter-basic.js');
 module.exports.JsonFilter = require('./lib/filter-json.js');
+
+module.exports.formatIsoDate = require('./lib/format-timestamp.js').formatIsoDate;
+module.exports.formatIsoDateUtc = require('./lib/format-timestamp.js').formatIsoDateUtc;
---
+++ test/test-format-timestamp.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2014 Andras Radics
+ * Licensed under the Apache License, Version 2.0
+ */
+
+assert = require('assert');
+
+formatIsoDate = require('../filters').formatIsoDate;
+formatIsoDateUtc = require('../filters').formatIsoDateUtc;
+pad2 = require('../lib/format-timestamp').pad2;
+pad3 = require('../lib/format-timestamp').pad3;
+pad4 = require('../lib/format-timestamp').pad4;
+
+
+module.exports = {
+    'should pad2': function(t) {
+        assert.equal(pad2(1), '01');
+        assert.equal(pad2(12), '12');
+        assert.equal(pad2(123), '123');
+        t.done();
+    },
+
+    'should pad3': function(t) {
+        assert.equal(pad3(1), '001');
+        assert.equal(pad3(12), '012');
+        assert.equal(pad3(123), '123');
+        assert.equal(pad3(1234), '1234');
+        t.done();
+    },
+
+    'should pad4': function(t) {
+        assert.equal(pad4(1), '0001');
+        assert.equal(pad4(12), '0012');
+        assert.equal(pad4(123), '0123');
+        assert.equal(pad4(1234), '01234');
+        assert.equal(pad4(12345), '12345');
+        t.done();
+    },
+
+    'should format ISO timestamp': function(t) {
+        var msg = formatIsoDate(980271296000);
+        assert.equal(msg, '2001-01-23 12:34:56');
+        t.done();
+    },
+
+    'should format ISO UTC timestamp': function(t) {
+        var msg = formatIsoDateUtc(980271296000);
+        assert.equal(msg, '2001-01-23 17:34:56');
+        t.done();
+    },
+};
