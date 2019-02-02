3954caedd135f02032e3223b4e071c28cb1d50ba
Andras <andras@andrasq.com>
2019-02-01 22:29:27 -0500

    speed up timestamp formatting, ...

--- filters.js
+++ filters.js
@@ -3,6 +3,7 @@ module.exports.filterBasic = require('./lib/filter-basic.js');
 module.exports.JsonFilter = require('./lib/filter-json.js');
 module.exports.BasicFilter = require('./lib/filter-basic.js').BasicFilter;
 
-module.exports.formatIsoDate = require('./lib/format-timestamp.js').formatIsoDate;
-module.exports.formatIsoDateUtc = require('./lib/format-timestamp.js').formatIsoDateUtc;
-module.exports.formatNumericDateUtc = require('./lib/format-timestamp.js').formatNumericDateUtc;
+var formatTimestamp = require('./lib/format-timestamp');
+module.exports.formatIsoDate = formatTimestamp.formatIsoDate;
+module.exports.formatIsoDateUtc = formatTimestamp.formatIsoDateUtc;
+module.exports.formatNumericDateUtc = formatTimestamp.formatNumericDateUtc;
