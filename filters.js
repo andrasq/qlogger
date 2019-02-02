cf092ff0cf466d3f188e220f51f68fc7f19e4436
Andras <andras@andrasq.com>
2019-02-01 20:43:10 -0500

    expose BasicFilter class, add create() builder to BasicFilter

--- filters.js
+++ filters.js
@@ -1,5 +1,7 @@
+// filterBasic is legacy
 module.exports.filterBasic = require('./lib/filter-basic.js');
 module.exports.JsonFilter = require('./lib/filter-json.js');
+module.exports.BasicFilter = require('./lib/filter-basic.js').BasicFilter;
 
 module.exports.formatIsoDate = require('./lib/format-timestamp.js').formatIsoDate;
 module.exports.formatIsoDateUtc = require('./lib/format-timestamp.js').formatIsoDateUtc;
