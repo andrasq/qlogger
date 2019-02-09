b9f40e9c280b2d9d25a5ee405c0920c3bf928a9a
Andras <andras@andrasq.com>
2019-02-09 11:37:49 -0500

    [skip ci] more benchmark timings

--- benchmark.js
+++ benchmark.js
@@ -8,6 +8,7 @@
 
 logfileName = '/tmp/test.log';          // also try /dev/shm/test.log
 useFilter = true;                       // 400k/s filtered, 650k/s raw; v1.4.0 i7 SKL: 700k/s filtered, 1100k/s raw
+                                        // v1.7.0 SKL 4.2g new DateFormatter w node-v0.10.42: 1000k/s, 1450k/s
 removeLogfile = true;                   // set to false to look at logfile contents
 
 
@@ -18,7 +19,9 @@ QLogger = require('./index');
 log = new QLogger('info', 'file://' + logfileName);
 if (useFilter) log.addFilter(require('./filters').filterBasic);
 //if (useFilter) log.addFilter(require('./filters').JsonFilter.create());
+//if (useFilter) log.addFilter(require('./filters').KubeFilter.create('test'));
 
+// umm... the 199 is a bug, should be 200, but has been legacy since 2014 (now 2019)
 message = new Array(199).join("x") + "\n";
 
 setImmediate = global.setImmediate || process.nextTick;
